fd63acecbe26130fe234f44f6aa2fa731ebe6c81
uisab <uisab@student.kit.edu>
2025-12-16 18:43:49 +0100

    Adjustments to accommodate an alternative function framework in the bounding module. This results in significant changes to the API and performance of the provided bounding procedures.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(working_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pyimpBB',
-    version='0.2.0',
+    version='0.2.1',
     description='Implementation of an algorithm from the field of global optimization according to the publication ‘The improvement function in branch-and-bound methods for complete global optimization’ by S. Schwarze, O. Stein, P. Kirst and M. Rodestock.',
     long_description=long_description,
     long_description_content_type='text/markdown',
